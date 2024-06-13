f1934ec3339c55a8c6a309ae014dd0295900c351
吴小白 <296015668@qq.com>
2024-06-13 10:03:15 +0800

    feat: 修改代码

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.proxyapp.uk": "https://registry-1.docker.io",
+  "quay.proxyapp.uk": "https://quay.io",
+  "gcr.proxyapp.uk": "https://gcr.io",
+  "k8s-gcr.proxyapp.uk": "https://k8s.gcr.io",
+  "k8s.proxyapp.uk": "https://registry.k8s.io",
+  "ghcr.proxyapp.uk": "https://ghcr.io",
+  "cloudsmith.proxyapp.uk": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
